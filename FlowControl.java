87b7a7f568178f64b5f354f8ae10912a05f7d633
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-01-10 00:02:36 +0530

    second time

--- FlowControl.java
+++ FlowControl.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class FlowControl {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        if(n>10){
+            System.out.println("Entered number is 10");
+        }
+        System.out.println("flow control : if statement");
+
+    }
+
 }
